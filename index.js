a42d07980e3f7985de245bb6ed29b19309eb6f9d
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-16 12:40:22 +1300

    traffic

---
+++ index.js
@@ -0,0 +1,19 @@
+var Red = 1;
+var Orange = 2;
+var Green = 3;
+
+let trafficLight = 1;
+
+if (trafficLight == Red){
+    console.log("Stop"); //will print "stop" if the condition is correct
+}
+
+else if (trafficLight == Orange){
+    console.log("slow-down");//will print "slow-down" if the condition is correct
+}
+
+else{
+    console.log("Go");//will print "Go" if the condition is anything else from the previous two.
+}
+
+
